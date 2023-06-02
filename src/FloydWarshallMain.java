277e12004268cba73fe3d789dfc37fb19d0da94c
Alyona Yaresko <63966600+nyavko@users.noreply.github.com>
2023-06-02 12:47:47 +0300

    Update FloydWarshallMain.java

--- src/FloydWarshallMain.java
+++ src/FloydWarshallMain.java
@@ -14,7 +14,7 @@ public class FloydWarshallMain {
     public static void main(String[] args) throws Exception {
         task curtask = new task();
         curtask.addJarFile("FloydWarshallModule.jar");
-        matrix = readData(curtask.findFile("input2.txt"));
+        matrix = readData(curtask.findFile("input.txt"));
 
         System.out.println("Vertices count: " + matrix.length);
 
